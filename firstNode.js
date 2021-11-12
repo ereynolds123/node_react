133345289d444803321bff85417b77534278bc9c
Emily Reynolds <emilyr@vermontsystems.com>
2021-11-12 09:33:47 -0500

    Built a simple server

--- firstNode.js
+++ firstNode.js
@@ -1,8 +1,22 @@
-function sum(a, b){
-    return a+b
-}
+//Import the sum function from the helpers file
+const helpers = require("./helpers")
 
-const total = sum(10,2);
+
+//Could also do const {sum}= require("./helpers"). This is destructuring
+
+const total = helpers.sum(10,2);
 
 //Total is 12
 console.log(total)
+
+//Creating a server that listens on port 3000
+const http = require("http");
+const port = process.env.PORT || 3000
+
+const app = http.createServer((req, res)=> {
+    res.end("Hello World from Node JS")
+
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
+
---
+++ helpers.js
@@ -0,0 +1,14 @@
+//Sums two variables together
+function sum(a, b){
+    return a+b
+}
+
+// //Arrow functions do not have their own context 
+// const sum = (a, b) =>{
+//     return a + b
+// }
+
+//Export the sum function 
+module.exports={
+    sum
+};
